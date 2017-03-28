f4fb850dc7590bbb37e8e4ee77594fdb8665aac0
aledj2 <aledjones@nhs.net>
2017-03-28 12:02:52 +0100

    add arguments and usual suspect list as inputs

--- README.md
+++ README.md
@@ -9,15 +9,19 @@ Amplivar creates a list of unique reads and then performs variant calling using 
 Analysis of cancer samples
 
 ## What data are required for this app to run?
-A list of fastq files.
+* A list of fastq files.
+* A list of usual suspects (by default this is the file saved in 001_ToolsReferenceData)
+* Values for Minimum reported variant frequency (optional - default = 20)
+* Values for Minimum coverage for variant calling (optional - default is to leave blank)
+* Values for Minimum number reads containing the variant allele (optional - default is to leave blank)
+* Value for Keepfiles (required - default = 1)
+  * 1= keep all files
+  * 2= keep files required for reanalysis from checkpoint 1  
+  * 3= keep only bam, vcf and log files and move the files into BAM, LOG, VCF files directories  
 
 ## What does this app output?
 * A list of vcfs (in a folder called vcfs)
 * One folder per sample including all the files created during analysis
-
-## Custom modifications
-* This app uses a fork of Amplivar (modified to use an updated version of VarScan)
-* The app was made by the Viapath Genome Informatics section 
 
 ## How does this app work?
 1. Clone the github repo
@@ -25,4 +29,9 @@ A list of fastq files.
 3. Create 2 bit reference
 4. Install cutadapt, Java
 5. Run BLAT server
-6. Run amplivar using -m Variant_Calling
+6. Download the list of usual suspects
+7. Run amplivar using -m Variant_Calling and arguments defined in inputs
+
+## Custom modifications
+* This app uses a fork of Amplivar modified to use an updated version of VarScan (https://github.com/moka-guys/amplivar)
+* The app was made by the Viapath Genome Informatics section 
--- src/code.sh
+++ src/code.sh
@@ -14,20 +14,24 @@ update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 #
 dx-download-all-inputs
 
+#read the api key as a variable
+API_KEY=$(cat '/home/dnanexus/auth_key')
+
 #make directory to hold input fastqs 
 mkdir to_test
 #make output folders
 mkdir -p out/vcfs/vcfs/ out/amplivar_out/amplivar_out
 
-#move all inputs
+#move all fastq inputs
 for input in /home/dnanexus/in/fastqs/*; do if [ -d "$input" ]; then mv $input/* to_test/; fi; done
 
-ls to_test
+#move (and rename) usual suspects list
+for file in /home/dnanexus/in/usual_suspects/*; do mv $file /home/dnanexus/usual_suspects.txt ; done
 
 #
 # Fetch and uncompress genome
 #
-#dx download 001_ToolsReferenceData:Data/ReferenceGenomes/hs37d5.fasta-index.tar.gz
+dx download 001_ToolsReferenceData:Data/ReferenceGenomes/hs37d5.fasta-index.tar.gz --auth $API_KEY
 tar -xvzf hs37d5.fasta-index.tar.gz
 
 # clone amplivar
@@ -35,24 +39,41 @@ git clone https://github.com/moka-guys/amplivar.git
 
 # install cutadapt
 sudo pip install cutadapt
-# git clone https://github.com/marcelm/cutadapt.git
-# cd cutadapt
-# python setup.py install
-# cd ..
 
 # make 2bit file
 amplivar/bin/linux/faToTwoBit genome.fa genome.2bit
 
+#set some variables used by amplivar
 export TERM=xterm
 export SHELL=/bin/bash
 
 # start blat server
 nohup /home/dnanexus/amplivar/bin/linux/gfServer start localhost 8800 /home/dnanexus/genome.2bit  &
 
-# run amplivar
-amplivar/bin/universal/amplivar_wrapper.sh -m VARIANT_CALLING -i /home/dnanexus/to_test  -o /home/dnanexus/out/amplivar_out/amplivar_out -s /home/dnanexus/amplivar/test/TruSeq_Cancer_genotype-lookup.txt -p /home/dnanexus/amplivar/test/TruSeq_Cancer_primer-flanks.txt -d TRUSEQ -t 3 -g /home/dnanexus/genome.fa -x localhost -y 8800 -1 20 -k 1
+#number of cores
+cores=$(nproc --all)
+cores=$(($cores-1))
 
-##copy the vcfs into a seperate output (potentially to feed into an annotator) - SHOULD THIS BE mv?
-for sample in /home/dnanexus/out/amplivar_out/amplivar_out/* ; do if [ -d "$sample" ]; then cp $sample/*.vcf /home/dnanexus/out/vcfs/vcfs/; fi; done
+#get the potential arguments for  Amplivar
+ opts=("-k" "$keepfiles")
+
+if [[ "$minfreq" != "" ]]
+	then
+	opts+=("-1" "$minfreq")
+fi
+if [[ "$mincov" != "" ]]
+	then
+	opts+=("-2" "$mincov")
+fi
+if [[ "$mincovvar" != "" ]]
+	then
+	opts+=("-3" "$mincovvar")
+fi
+
+# run amplivar
+amplivar/bin/universal/amplivar_wrapper.sh -m VARIANT_CALLING -i /home/dnanexus/to_test  -o /home/dnanexus/out/amplivar_out/amplivar_out -s /home/dnanexus/usual_suspects.txt -p /home/dnanexus/amplivar/test/TruSeq_Cancer_primer-flanks.txt -d TRUSEQ -t $cores -g /home/dnanexus/genome.fa -x localhost -y 8800 "${opts[@]}"
+
+##move the vcfs into a seperate output (potentially to feed into an annotator) 
+for sample in /home/dnanexus/out/amplivar_out/amplivar_out/* ; do if [ -d "$sample" ]; then mv $sample/*.vcf /home/dnanexus/out/vcfs/vcfs/; fi; done
 
 dx-upload-all-outputs --parallel
